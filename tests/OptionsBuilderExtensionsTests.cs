59cf62b5747ce3b3d296cdaf2e653d665eb0651a
kolan72 <kolan72@gmail.com>
2026-03-31 17:27:11 +0300

    Update libraries used in tests.

--- tests/OptionsBuilderExtensionsTests.cs
+++ tests/OptionsBuilderExtensionsTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using System.Linq;
 
 namespace ExpressValidator.Extensions.ValidationOnStart.Tests
 {
